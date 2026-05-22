41f2dd3f7c5435a083e5ab0efb338d0e277d936b
XRRAKESH <xz8100487734@gmail.com>
2026-05-22 19:26:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ async def json_to_proto(json_data: str, proto_message: Message) -> bytes:
 def get_account_credentials(region: str) -> str:
     r = region.upper()
     if r == "IND":
-        return "uid=4917065953&password=MUTDUKI_JAHID_X_EMPIRE_yFLPuaJ0"
+        return "uid=4822068511&password=MUTSUKI_JAHID_X_EMPIRE_bUadZ0ij"
     elif r == "ME":
         return "uid=4925612754&password=K3INUKOTT67SUIS7AU5O6UAUKA235TIIO6KEIN93AUSUE42AUT7MMISU0TSSI163"
     elif r in {"BR", "US", "SAC", "NA"}:
